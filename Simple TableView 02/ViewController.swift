991d2d742811a7ee9aef7a7ca5ee69d3e0b2bf56
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-04-30 15:30:24 +0900

    simple table view

--- Simple TableView 02/ViewController.swift
+++ Simple TableView 02/ViewController.swift
@@ -8,11 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let  identifier = "RE"
+        let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        cell.textLabel?.text = animals[indexPath.row]
+        
+        return cell
+    }
+    
 
+    @IBOutlet weak var myTableView: UITableView!
+    var animals = ["COW", "HORSE" , "MOUSE" , "PIG" , "TIGER" , "LION"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
